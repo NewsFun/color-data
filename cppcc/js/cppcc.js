72ca28a505cc04fbcfd79abc1d5b1614cbb1fbc8
newsfun <80733049@qq.com>
2017-02-22 19:29:59 +0800

    大风车动画

--- cppcc/js/cppcc.js
+++ cppcc/js/cppcc.js
@@ -61,28 +61,68 @@
         }
         //chatitem.eq(n).addClass('move-target');
     }
-    function randomMove(){
-        setInterval(function(){
-            var num = ~~(Math.random()*4+1);
-            loopMove(num);
-        }, 5000);
-    }
-    randomMove();
+    //randomMove();
+
     /*摩天轮*/
     var skywheel = $('#sky-wheel'),
         wheels = skywheel.children('.bo-wheel-item'),
         number = wheels.length;
     var SIN = Math.sin, COS = Math.cos, RAD = Math.PI/180;
-    var r0 = skywheel.outerHeight(), r1 = wheels.first().outerHeight(), r = ~~((r0-r1)/2), angle = (360/number);
+    var r0 = skywheel.outerHeight(), r1 = wheels.first().outerHeight(), r = ~~((r0-r1)/2), angle = (360/number), clock = 0, state = 0, _rad = 0;
+    var Animation = {
+        rotation:function(){
+            var tick = setInterval(function(){
+                if(_rad<angle){
+                    clock ++;/*动作执行部分*/
+                    _rad ++;
+                    setPosition(countPosition(clock));
+                }else{
+                    clearInterval(tick);/*动作结束*/
+                    _rad = 0;
+                }
+            },1000/fps);
+        },
+        moveTo:function(start, end){
+            for(var i = 0;i<number;i++){
+                var dx = end[i].left-start[i].left;
+                var dy = end[i].top-start[i].top;
+                var distance = Math.sqrt(dx*dx+dy*dy);
+                if(distance>1){
+                    start[i].left += dx*0.1;
+                    start[i].top += dy*0.1;
+                }else{
+                    state++;
+                }
+            }
+        }
+    };
     function initSkyWheel(){
-
+        var start = [];
+        wheels.each(function(){
+            start.push({'left':283, 'top':283});
+        });
+        var end = countPosition();
+        moveToCircle(start, end);
+    }
+    function moveToCircle(start, end){
+        var tick = setInterval(function(){
+            if(state<number){
+                Animation.moveTo(start, end);
+                setPosition(start);
+            }else{
+                clearInterval(tick);
+                //Animation.rotation();
+                randomMove();
+            }
+        },1000/fps);
     }
     function setPosition(array){
         wheels.each(function(v, k){
             $(this).css('left', array[v].left).css('top', array[v].top);
+            array[v].left>550?$(this).addClass('on'):$(this).removeClass('on');/*判断是否进入了focus内*/
         });
     }
-    function calculate(reg){
+    function countPosition(reg){
         var pos = [], clock = reg||0;
         for(var i = 0;i<number;i++){
             pos.push({
@@ -92,21 +132,13 @@
         }
         return pos;
     }
-    function rotation(){
-        function animate(){
-            var rad = 0;
-            var tick = setInterval(function(){
-                if(rad<angle){
-                    rad ++;/*动作执行部分*/
-                    calculate(rad);
-                }else{
-                    clearInterval(tick);/*动作结束*/
-                }
-            },1000/fps);
-        }
-        animate();
+    function randomMove(){
+        setInterval(function(){
+            var num = ~~(Math.random()*4+1);
+            loopMove(num);
+            Animation.rotation();
+        }, 5000);
     }
-    setPosition();
-    rotation();
+    initSkyWheel();
 
 })(this);
