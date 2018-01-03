f1560e069b77d09a21231832747cdc2cf36a5cd4
newsfun <root@fengdeMacBook-Pro.local>
2018-01-03 19:28:38 +0800

    make file canvas.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Canvas from '@/components/canvas';
 import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
@@ -8,6 +9,10 @@ export default new Router({
     routes: [
         {
             path: '/',
+            name: 'canvas',
+            component: Canvas
+        },{
+            path: '/hello-world',
             name: 'HelloWorld',
             component: HelloWorld
         }
