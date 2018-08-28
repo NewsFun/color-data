d09323db8cb6f4eb8d315c5dd3772a7586387b94
bobo <80733049@qq.com>
2018-08-28 14:39:39 +0800

    add: redux 2.1

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -1,133 +1,135 @@
 <template>
-    <canvas id="canvas" ref="canvas">
-        <img id="star" 
-            src="../resources/flystar.png" 
-            style="display:none" 
-            alt="">
-    </canvas>
+  <canvas id="canvas" ref="canvas">
+    <img id="star" 
+      src="../assets/flystar.png" 
+      style="display:none" 
+      alt="">
+  </canvas>
 </template>
+
 <script>
-import { Stage } from './stage';
+import { Stage } from "./stage";
 
 const tars = [
-    { x:200, y:400 },
-    { x:500, y:500 },
-    { x:100, y:300 },
-    { x:200, y:50 }
+  { x: 200, y: 400 },
+  { x: 500, y: 500 },
+  { x: 100, y: 300 },
+  { x: 200, y: 50 }
 ];
 const moveList = [
-    {
-        endPos: tars[0],
-        type: 'linear',
-        wait: 60,
-        ve: 10
-    },{
-        endPos: tars[1],
-        type: 'linear',
-        ve: 5
-    },{
-        endPos: tars[2],
-        type: 'linear',
-        ve: 10
-    },{
-        endPos: tars[3],
-        type: 'linear',
-        ve: 5
-    }
+  {
+    endPos: tars[0],
+    type: "linear",
+    wait: 60,
+    ve: 10
+  },
+  {
+    endPos: tars[1],
+    type: "linear",
+    ve: 5
+  },
+  {
+    endPos: tars[2],
+    type: "linear",
+    ve: 10
+  },
+  {
+    endPos: tars[3],
+    type: "linear",
+    ve: 5
+  }
 ];
 
 export default {
-    data(){
-        return {
-            img: null,
-            ctx: null,
-            stage: null,
-            canvas: null,
-            W: window.innerWidth,
-            H: window.innerHeight
+  data() {
+    return {
+      img: null,
+      ctx: null,
+      stage: null,
+      canvas: null,
+      W: window.innerWidth,
+      H: window.innerHeight
+    };
+  },
+  methods: {
+    initStars(img) {
+      let stage = this.stage;
+      let center = { x: 100, y: 150 };
+
+      stage
+        .setActors({
+          key: "star1",
+          img: img,
+          coord: {
+            x: 200,
+            y: 50
+          }
+        })
+        .setMotion(moveList);
+
+      stage
+        .setActors({
+          key: "star2",
+          shape: "round",
+          coord: {
+            x: 100,
+            y: 100
+          }
+        })
+        .setMotion([
+          {
+            type: "circling",
+            endPos: center,
+            radian: 180,
+            va: 1
+          }
+        ]);
+      stage.setActors({
+        key: "star3",
+        shape: "round",
+        coord: center
+      });
+      this.stage.animate();
+    },
+    setCanvas() {
+      let stage = new Stage();
+      let ctx = stage.ctx;
+
+      this.stage = stage;
+      this.ctx = ctx;
+
+      return ctx;
+    },
+    imgLoad(img) {
+      let me = this;
+      if (img.complete) {
+        me.img = img;
+      } else {
+        img.onload = function() {
+          me.img = img;
         };
+      }
     },
-    methods:{
-        initStars(img) {
-            let stage = this.stage;
-            let center = { x: 100, y: 150 };
-            
-            stage.setActors({
-                key:'star1',
-                img: img, 
-                coord:{
-                    x: 200,
-                    y: 50
-                }
-            }).setMotion(moveList);
-
-            stage.setActors({
-                key:'star2',
-                shape: 'round',
-                coord:{
-                    x: 100,
-                    y: 100
-                }
-            }).setMotion([{
-                type: 'circling',
-                endPos: center,
-                radian: 180,
-                va: 1
-            }]);
-            stage.setActors({
-                key:'star3',
-                shape: 'round',
-                coord: center
-            });
-            // stage.getActors('star2').motions[0].attention = true;
-            // console.log(sam);
-            this.stage.animate();
-        },
-        setCanvas(){
-            let stage = new Stage();
-            let ctx = stage.ctx;
-
-            this.stage = stage;
-            this.ctx = ctx;
-
-            return ctx;
-        },
-        imgLoad(img){
-            let me = this;
-            if(img.complete){
-                me.img = img;
-            }else{
-                img.onload = function(){
-                    me.img = img;
-                };
-            }
-        },
-        showPoint(point){
-            let ctx = this.ctx;
-            
-            ctx.save();
-            ctx.fillStyle = '#00ff00';
-            ctx.arc(point.x, point.y, 2, 0, Math.PI*2);
-            ctx.fill();
-            ctx.restore();
-        }
-    },
-    mounted(){
-        let img = document.querySelector('#star');
-        this.imgLoad(img);
-        this.ctx = this.setCanvas();
-    },
-    created(){
-        
-    },
-    watch:{
-        img(val){
-            this.initStars(val);
-        }
+    showPoint(point) {
+      let ctx = this.ctx;
+      ctx.save();
+      ctx.fillStyle = "#00ff00";
+      ctx.arc(point.x, point.y, 2, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.restore();
     }
+  },
+  mounted() {
+    let img = document.querySelector("#star");
+    this.imgLoad(img);
+    this.ctx = this.setCanvas();
+  },
+  created() {},
+  watch: {
+    img(val) {
+      this.initStars(val);
+    }
+  }
 };
 </script>
-<style lang="sass" scoped>
-
-</style>
 
--- src/components/motion.js
+++ src/components/motion.js
@@ -5,16 +5,16 @@ const ASIN = Math.asin;
 const ROUND = Math.round;
 
 export class Motion {
-    constructor(arg){
+    constructor(arg) {
         const config = {
-            coord:{
-                x : 100,
-                y : 100
+            coord: {
+                x: 100,
+                y: 100
             },
-            vx : 1,
-            vy : 1,
-            ve : 1,
-            va : 1,
+            vx: 1,
+            vy: 1,
+            ve: 1,
+            va: 1,
             wait: 0,
             errorRange: 1
         };
@@ -22,26 +22,26 @@ export class Motion {
         this.t = 0;
         this.distance = null;
         this.attention = false;
-        
+
         let option = Object.assign(config, arg);
         this.initData(option);
     }
     // 初始化数据
-    initData(option){
+    initData(option) {
         Object.assign(this, option);
-        
-        if(this.type==='circling'){
+
+        if (this.type === 'circling') {
             this.radius = ROUND(this.getDistance(option.endPos));
         }
     }
     // 刹车
-    arrived(distance){
-        if(distance<this.errorRange) return true;
+    arrived(distance) {
+        if (distance < this.errorRange) return true;
 
-        if(this.distance===null){
+        if (this.distance === null) {
             this.distance = distance;
-        }else{
-            if(this.distance === distance){
+        } else {
+            if (this.distance === distance) {
                 this.distance = null;
                 return true;
             }
@@ -50,105 +50,107 @@ export class Motion {
         return false;
     }
     // 直线反弹
-    bounce(minWidth, maxWidth, minHeight, maxHeight){
-        
-        if(this.coord.x <= minWidth){
+    bounce(minWidth, maxWidth, minHeight, maxHeight) {
+
+        if (this.coord.x <= minWidth) {
             this.vx = -this.vx;
             this.coord.x = minWidth;
         }
-        if(this.coord.x >= maxWidth){
+        if (this.coord.x >= maxWidth) {
             this.vx = -this.vx;
             this.coord.x = maxWidth;
         }
-        if(this.coord.y <= minHeight){
+        if (this.coord.y <= minHeight) {
             this.vy = -this.vy;
             this.coord.y = minHeight;
         }
-        if(this.coord.y >= maxHeight){
+        if (this.coord.y >= maxHeight) {
             this.vy = -this.vy;
             this.coord.y = maxHeight;
         }
-        
+
         return this;
     }
     // 圆周运动
-    circling(radian=0){
+    circling(radian = 0) {
         let rads = ~~radian;
-        let rad = PI*this.t/180;
+        let rad = PI * this.t / 180;
         // console.log(radian);
-        this.coord.x = this.endPos.x + COS(rad)*this.radius;
-        this.coord.y = this.endPos.y + SIN(rad)*this.radius;
-        if(rads>0&&this.t>=rads){
+        this.coord.x = this.endPos.x + COS(rad) * this.radius;
+        this.coord.y = this.endPos.y + SIN(rad) * this.radius;
+        if (rads > 0 && this.t >= rads) {
             this.attention = true;
         }
         this.t += this.va;
     }
     // 红灯
     delay() {
-        if(this.wait>0){
+        if (this.wait > 0) {
             this.wait -= 1;
-        }else{
+        } else {
             this.wait = 0;
         }
-        return this.wait>0;
+        return this.wait > 0;
     }
     // 获取坐标差和距离
     getDistance(n, details) {
         let dx = n.x - this.coord.x;
         let dy = n.y - this.coord.y;
-        let d = Math.sqrt( dx*dx + dy*dy );
-        
+        let d = Math.sqrt(dx * dx + dy * dy);
+
         return details ? [dx, dy, d] : d;
     }
     // 获取初始化夹角
-    getInitAngle(center){
+    getInitAngle(center) {
         let d = this.getDistance(center, true);
-        let sin = d[0]/d[2];
+        let sin = d[0] / d[2];
         let ang = ASIN(sin);
         return [ang, d[2]];
     }
     // 运动流程控制
-    movement(){
-        if(!this.attention&&!this.delay()) this.selectMoveType();
+    movement() {
+        if (!this.attention && !this.delay()) this.selectMoveType();
         return this.coord;
     }
     // 直线运动
     rectilinear(endPos) {
         let vel = this.getDistance(endPos, true);
-        if(!this.attention) this.attention = this.arrived(vel[2]);
-        if(!this.attention){
-            this.vx = vel[0]/vel[2]*this.ve;
-            this.vy = vel[1]/vel[2]*this.ve;
-        }else{
+        if (!this.attention) this.attention = this.arrived(vel[2]);
+        if (!this.attention) {
+            this.vx = vel[0] / vel[2] * this.ve;
+            this.vy = vel[1] / vel[2] * this.ve;
+        } else {
             this.vx = 0;
             this.vy = 0;
         }
         return this;
     }
     // 路径类型
-    selectMoveType(){
-        if(!this.type) console.error('you need defined a "type" to start Motion!');
-        
-        switch(this.type){
-            case 'linear': 
+    selectMoveType() {
+        if (!this.type) console.error('you need defined a "type" to start Motion!');
+
+        switch (this.type) {
+            case 'linear':
                 this.rectilinear(this.endPos).update();
                 break;
-            case 'bounce': {
-                let minw = this.minWidth;
-                let maxw = this.maxWidth;
-                let minh = this.minHeight;
-                let maxh = this.maxHeight;
-                this.bounce(minw, maxw, minh, maxh).update();
-            }
+            case 'bounce':
+                {
+                    let minw = this.minWidth;
+                    let maxw = this.maxWidth;
+                    let minh = this.minHeight;
+                    let maxh = this.maxHeight;
+                    this.bounce(minw, maxw, minh, maxh).update();
+                }
                 break;
             case 'circling':
                 this.circling(this.radian);
                 break;
-            default: break;
+            default:
+                break;
         }
     }
     // 更新坐标
-    update(){
+    update() {
         this.coord.x += this.vx;
         this.coord.y += this.vy;
         this.t += 1;
